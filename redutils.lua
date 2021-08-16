c042fb7776b41c714a24cb8ca41a5e611c9cc339
4498Lincoln <49529648+4498Lincoln@users.noreply.github.com>
2021-08-15 17:46:43 -0700

    Timeout functionality on sreceive()
    Needs testing... but it should be fine.

--- redutils.lua
+++ redutils.lua
@@ -1,8 +1,18 @@
-function sreceive(prot, sid)
+function sreceive(prot, sid, timeouttime)
+    local timeouttime = timeouttime or math.huge
+    local deadline = os.clock() + timeouttime
     while true do
-        local senderId, message, protocol = rednet.receive(prot)
+        local timelimit = deadline - os.clock()
+        if timelimit <= 0 then
+            -- Timed out... somehow
+            return nil
+        end
+        local senderId, message, protocol = rednet.receive(prot, timelimit)
         if senderId == sid then
             return senderId, message, protocol
+        elseif not senderId then
+            -- Timed out
+            return nil
         end
     end
 end
